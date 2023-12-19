d971ee1c57e00c915519345c16eb43092e638bd2
SubbaReddy-Subbu <149754454+SubbaReddy-Subbu@users.noreply.github.com>
2023-12-19 10:12:26 +0530

    small

--- netflix-clone/components/Billboard.tsx
+++ netflix-clone/components/Billboard.tsx
@@ -41,7 +41,7 @@ const Billboard = () => {
         <PlayButton movieId={data?.id}/>
         <button onClick={handleopenModal} className="bg-white flex text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex-row items-center hover:bg-opacity-20 transition"
         >
-            <InformationCircleIcon  className="mr-1"/>More Info
+            <InformationCircleIcon  className="mr-1 w-8"/>More Info
         </button>
       </div>
       </div>
